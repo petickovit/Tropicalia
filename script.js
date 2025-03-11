c2ffc66fc4cadc15516589b7ff098710cf9641d2
petickovit <160601349+petickovit@users.noreply.github.com>
2025-03-11 09:47:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ diminuirFonteBotao.addEventListener ('click', function(){
 })
 const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
 const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-botaoDeAcessibilidade.addEventListener('click',function(){
+botaoDeAcessibilidade.addEventListener ('click',function(){
 botaoDeAcessibilidade.classList.toggle('rotacao-botao');
 opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
 
