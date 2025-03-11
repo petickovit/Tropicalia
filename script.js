5b41d97e6bac9d560561fd3d8952a2d4d7259106
petickovit <160601349+petickovit@users.noreply.github.com>
2025-03-11 09:35:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 const diminuirFonteBotao = document.getElementById('dimunir-fonte');
 let tamanhoAtualFonte = 1;
 aumentaFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte = 1;
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = '${tamanhoAtualFonte} rem'
+})
+diminuirFonteBotao.addEventListener ('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = '${tamanhoAtualFonte} rem'
+})
+})
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+botaoDeAcessibilidade.addEventListener('click',function(){
+botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
 })
