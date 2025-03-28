4c41e630302e137c40efc4c5235f983bea615037
petickovit <160601349+petickovit@users.noreply.github.com>
2025-03-28 09:39:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-document.addEventListener('DOMContentLoaded',function (){
-    const aumentaFonteBotao = document.getElementById
-    ('aumentar-fonte');
-    const diminuirFonteBotao = document.getElementById
-    ('diminuir-fonte');
-    const alternaContraste = document.getElementById
-    ('alterna-contraste');
-     let tamanhoAtualFonte = 1;
+document.addEventListener ('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
-       aumentaFonteBotao.addEventListener('click',function(){
-         tamanhoAtualFonte += 0.1;
-          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-        })
-       diminuirFonteBotao.addEventListener('click',function(){
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+    })
+
+    const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById ('diminuir-fonte');
+    const alternaContraste = document.getElementById ('alterna-contraste');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    diminuirFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-       })
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-const opcoesDeAcesibilidade = document.getElementById('opcoes-acessibilidade');
-  botaoDeAcessibilidade.addEventListener('click', function (){
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcesibilidade.classList.toggle('apresenta-lista');
-    const botaoSelecionado = botaoDeAcessibilidade.getAttribute
-    ('aria-expanded')== 'true';
-    botaoDeAcessibilidade.setAttribute('aria-expanded',
-    !botaoSelecionado);
-  })
-  alternaContraste.addEventListener('click',function() {
-    document.body.classList.toggle('alto-contraste')
-  })
- })
- 
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+})
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
